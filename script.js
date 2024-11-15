f8f3c3d6c2c678f73cc7ad9e4a90763c6f08ac3e
Aleksanteri Nazarenko <aleksanterinazarenko@gmail.com>
2024-11-15 17:14:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,19 @@ window.onload = () => {
     displayRandomWord();
     document.getElementById('randomize-button').onclick = randomizeWord;
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+  });
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.nav-list a');
+    const currentPath = window.location.pathname.split("/").pop();
+  
+    navLinks.forEach(link => {
+        const linkPath = link.getAttribute('href');
+        if (linkPath === currentPath) {
+            link.classList.add('active');
+        }
+    });
+  });
