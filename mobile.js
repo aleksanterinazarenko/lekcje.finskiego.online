108166c2f5b755354187695fa2d1e9a5b04d10fd
Aleksanteri Nazarenko <aleksanterinazarenko@gmail.com>
2024-11-19 23:33:53 +0100

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -53,19 +53,22 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 function setBodyHeight() {
-    const windowHeight = window.innerHeight;
-
-    document.body.style.height = `${windowHeight}px`;
-    document.body.style.overflow = "hidden";
+    document.body.style.height = `${window.innerHeight}px`;
 }
 
 setBodyHeight();
 
-window.addEventListener("resize", setBodyHeight);
+window.addEventListener('resize', setBodyHeight);
 
-// Ustawienia dla tła
-document.body.style.backgroundImage = "url('background.png')";
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundPosition = "right top";
-document.body.style.backgroundSize = `auto 100%`;
-document.body.style.backgroundAttachment = "scroll";
+const backgroundDiv = document.createElement("div");
+backgroundDiv.style.position = "fixed";
+backgroundDiv.style.top = "0";
+backgroundDiv.style.right = "0";
+backgroundDiv.style.width = "100%";
+backgroundDiv.style.height = `${window.innerHeight}px`;
+backgroundDiv.style.backgroundImage = "url('background.png')";
+backgroundDiv.style.backgroundRepeat = "no-repeat";
+backgroundDiv.style.backgroundPosition = "right top";
+backgroundDiv.style.backgroundSize = "auto 100%";
+backgroundDiv.style.zIndex = "-1";
+document.body.appendChild(backgroundDiv);
