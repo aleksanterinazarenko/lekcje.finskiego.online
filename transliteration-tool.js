14a5a10868250b13e1ef25312e29338a7884df51
Aleksanteri Nazarenko <aleksanterinazarenko@gmail.com>
2025-01-23 17:59:56 +0100

    Add files via upload

---
+++ transliteration-tool.js
@@ -0,0 +1,99 @@
+document.getElementById('inputText').addEventListener('input', function() {
+    updateOutputText();
+});
+
+function updateOutputText() {
+    const inputText = document.getElementById('inputText').value;
+    const outputText = transliterate(inputText);
+    document.getElementById('outputText').value = outputText;
+}
+
+export { transliterateToLatin };
+
+document.getElementById('copyButton').addEventListener('click', function() {
+    const outputText = document.getElementById('outputText');
+    outputText.select();
+    outputText.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    alert('Text copied to clipboard');
+});
+
+const letters = {
+    "А": "A", "Б": "B", "В": "V", "Г": "G", "Д": "D", "Е": "E", "Ё": "O", "Ж": "Ž", "З": "Z", "И": "I", "Й": "J",
+    "К": "K", "Л": "L", "М": "M", "Н": "N", "О": "O", "П": "P", "Р": "R", "С": "S", "Т": "T", "У": "U", "Ф": "F",
+    "Х": "H", "Ц": "C", "Ч": "Č", "Ш": "Š", "Щ": "Šč", "Ъ": "", "Ы": "Ï", "Ь": "", "Э": "Ë", "Ю": "U", "Я": "A",
+    "Ӕ": "Ä", "Ѣ": "E", "Ҥ": "N", "І": "I", "Ѳ": "Kv",
+    "а": "a", "б": "b", "в": "v", "г": "g", "д": "d", "е": "e", "ё": "o", "ж": "ž", "з": "z", "и": "i", "й": "j",
+    "к": "k", "л": "l", "м": "m", "н": "n", "о": "o", "п": "p", "р": "r", "с": "s", "т": "t", "у": "u", "ф": "f",
+    "х": "h", "ц": "c", "ч": "č", "ш": "š", "щ": "šč", "ь": "", "ъ": "", "ы": "ï", "э": "ë", "ю": "u", "я": "a",
+    "ӕ": "ä", "ѣ": "e", "ҥ": "n", "і": "i", "ѳ": "kv"
+};
+
+const palatals = {
+    "Д": "Ď", "З": "Ź", "Л": "Ľ", "Н": "Ń", "Р": "Ŕ", "С": "Ś", "Т": "Ť", "Ц": "Ć",
+    "д": "ď", "з": "ź", "л": "ľ", "н": "ń", "р": "ŕ", "с": "ś", "т": "ť", "ц": "ć"
+};
+
+const vowels = "АОУЫЭЯЁЮИЕЪЬаоуыэяёюиеъь";
+const front = "ЕЁИЮЯеёиюя";
+const nondentals = "БВГЖКПМШбвгжкпмш";
+
+function transliterate(text) {
+    text = " " + text + " ";
+
+    text = text.replace(/([дзлнрстц])ь(?=[дзнрстхц]+[еёияіь])/g, '$1');
+    text = text.replace(/([дзлнрстц])ь(?=[дзнрстхц]+[эы])/g, '$1');
+    text = text.replace(/([дзлнрстц])ь(?=[еёияіь])/g, '$1j');
+
+    text = text.replace(/([\u0301])/g, '');
+
+    text = text.replace(/([е])([и])([я])([і])/g, '$1j$2j$3');
+    text = text.replace(/([аоуыэяёюиеъьіӕ])([еёюя])/g, '$1j$2');
+    text = text.replace(/([АОУЫЭЯЁЮИЕЪЬІӔ])([еёюя])/g, '$1j$2');
+    text = text.replace(/([аоуыэяёюиеъіӕ])([иі])/g, '$1j$2');
+    text = text.replace(/([АОУЫЭЯЁЮИЕЪІӔ])([иі])/g, '$1j$2');
+
+    text = text.replace(/(^|\s|[.,!?;:()\[\]{}"“”«»\-])([ёюя])/g, '$1j$2');
+    text = text.replace(/(^|\s|[.,!?;:()\[\]{}"“”«»\-])([ЁЮЯ])/g, '$1J$2');
+    text = text.replace(/-([ёюя])/g, '-j$1');
+    text = text.replace(/-([ЁЮЯ])/g, '-J$1');
+
+    text = text.replace(/([аеёиоуыэюя])э/g, '$1e')
+    text = text.replace(/([АЕЁИОУЫЭЮЯ])Э/g, '$1e')
+    text = text.replace(/(^|\s|[.,!?;:()\[\]{}"“”«»\-])э/g, '$1e');
+    text = text.replace(/(^|\s|[.,!?;:()\[\]{}"“”«»\-])Э/g, '$1E');
+    text = text.replace(/(^|\s|[.,!?;:()\[\]{}"“”«»\-])і/g, '$1i');
+    text = text.replace(/(^|\s|[.,!?;:()\[\]{}"“”«»\-])І/g, '$1I');
+
+    text = text.replace(/([бвгджзйклмнпрстфхцчшщ])я/g, '$1ä');
+    text = text.replace(/([БВГДЖЗЙКЛМНПРСТФХЦЧШЩ])я/g, '$1ä');
+    text = text.replace(/([ДЗЛНРСТЦдзлнрстц])е/g, '$1e');
+    text = text.replace(/([ДЗЛНРСТЦдзлнрстц])и/g, '$1i');
+
+    text = text.replace(/([бвгжкмпфхчшщ])ь/g, '$1');
+    text = text.replace(/ъ/g, '');
+    text = text.replace(/(\s|^)і/g, '$1j');
+    text = text.replace(/і([аеёиоуыэюяАЕЁИОУЫЭЮЯ])/g, 'j$1');
+    text = text.replace(/й([аеёиоуыэюяАЕЁИОУЫЭЮЯ])/g, 'j-$1');
+    text = text.replace(/([БВГЖКПМШбвгжкпмш])ь/g, '$1');
+    
+    text = text.replace(/И/g, 'I');
+    text = text.replace(/Е/g, 'E');
+    text = text.replace(/([ИЕЁЮЯ])([^А-Я])/g, function(match, p1, p2) {
+        return p1.toLowerCase() + p2;
+    });
+
+    text = text.replace(/«/g, '„').replace(/»/g, '“');
+
+for (const [key, value] of Object.entries(palatals)) {
+    const regexVowel = new RegExp(key + 'ь([аеёиоуыэюяАЕЁИОУЫЭЮЯ])', 'g');
+    text = text.replace(regexVowel, value + '-$1');
+
+    const regexNoVowel = new RegExp(key + 'ь', 'g');
+    text = text.replace(regexNoVowel, value);
+
+    text = text.replace(new RegExp(key + `([${front}])`, 'g'), value + '$1');
+}
+
+    return text.trim().split('').map(char => letters[char] || char).join('');
+}
