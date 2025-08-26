8131a8910aa9276b238137492d79d9bfd695c56c
Aleksanteri Nazarenko <aleksanterinazarenko@gmail.com>
2025-08-26 22:27:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,26 @@
-async function fetchWords() {
-    const response = await fetch('words.json');
-    const words = await response.json();
-    return words;
-}
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM fully loaded and parsed");
+});
 
-function getRandomWord(words) {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    return words[randomIndex];
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-list a');
+  const currentPath = window.location.pathname.split("/").pop();
 
-async function displayRandomWord() {
-    const words = await fetchWords();
-    const randomWord = getRandomWord(words);
-    
-    document.getElementById('english').textContent = randomWord.English;
-    document.getElementById('finnish').textContent = randomWord.Finnish;
-    document.getElementById('polish').textContent = randomWord.Polish;
-}
-
-window.onload = displayRandomWord;
-
-async function randomizeWord() {
-    const words = await fetchWords();
-    const randomWord = getRandomWord(words);
-
-    setTimeout(() => {
-        document.getElementById('english').textContent = randomWord.English;
-        document.getElementById('finnish').textContent = randomWord.Finnish;
-        document.getElementById('polish').textContent = randomWord.Polish;
-    }, 100);
-}
-
-window.onload = () => {
-    displayRandomWord();
-    document.getElementById('randomize-button').onclick = randomizeWord;
-};
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
+  navLinks.forEach(link => {
+    const linkPath = link.getAttribute('href');
+    if (linkPath === currentPath) {
+      link.classList.add('active');
+    }
   });
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('.nav-list a');
-    const currentPath = window.location.pathname.split("/").pop();
-  
-    navLinks.forEach(link => {
-        const linkPath = link.getAttribute('href');
-        if (linkPath === currentPath) {
-            link.classList.add('active');
-        }
-    });
-  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  function replaceInTextNodes(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      node.nodeValue = node.nodeValue.replace(/\u2002/g, '\u00A0');
+    } else {
+      node.childNodes.forEach(replaceInTextNodes);
+    }
+  }
+  replaceInTextNodes(document.body);
+});
